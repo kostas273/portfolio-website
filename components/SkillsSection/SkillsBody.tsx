8d22165eb38d3ccdf382842a450908747e6e3ec2
Konstantinos Politis <politisk273@gmail.com>
2023-08-16 23:31:45 +0300

    source code anchor link fix

--- components/SkillsSection/SkillsBody.tsx
+++ components/SkillsSection/SkillsBody.tsx
@@ -8,7 +8,7 @@ export default function SkillsBody() {
       <p className="py-2 md:py-0">
         As a Junior Software Engineer, I bring a diverse skill set and a strong passion for crafting meaningful software solutions. I have gained valuable hands-on experience in building robust end-to-end test automation frameworks, Web APIs and automating tasks to streamline processes. I also enjoy the creative process of building user-friendly interfaces that seamlessly adapt to various devices and screen sizes, enhancing the overall user experience.
       </p>
-      <a className='underline cursor-pointer' href='https://github.com/kostas273/portfolio-website.git'>Check out out the source code</a>
+      <a className='underline cursor-pointer' href='https://github.com/kostas273/portfolio-website.git' target="_blank">Check out out the source code</a>
     </div>
   )
 }
